ecca9a06abd4d92f4ab6b79ec84337f58cc11f41
hulixiaoyao <18182011446@163.com>
2025-03-24 22:51:54 +0800

    2025.3.24王道c语言练习

---
+++ 5.2.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main(){
+  char str[100];
+  gets(str);
+  int len =strlen(str);
+  char reverse[len];
+  for(int i=0;i<len;i++){
+    reverse[i]=str[len-i-1];
+    }
+    reverse[len]='\0';
+    int result=strcmp(str,reverse);
+    if(result==0){
+      printf("0");
+      }
+    else if(result<0){
+        printf("-1");}
+    else{
+          printf("1");
+          }
+          return 0;
+          }
