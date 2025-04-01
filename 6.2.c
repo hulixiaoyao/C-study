163dcd3917445d56f6c72caabfdca33d9a7cc961
hulixiaoyao <18182011446@163.com>
2025-04-01 21:59:53 +0800

    2025.4.1代码练习

---
+++ 6.2.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    int i;
+    char c;
+    scanf("%d",&i);
+    char *p;
+    p=(char*)malloc(i);
+    scanf("%c",&c);
+    fgets(p,i,stdin);
+    puts(p);
+    free(p);
+    return 0;
+}
