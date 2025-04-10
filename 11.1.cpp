c59916010c6115a298d02ad59bb77b680b6db8a6
hulixiaoyao <18182011446@163.com>
2025-04-10 22:55:23 +0800

    2025.4.10王道c

---
+++ 11.1.cpp
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElemType;
+typedef struct LNode{
+    ElemType data;
+    struct LNode *next;
+}LNode,*LinkList;
+bool  list_head_insert (LinkList &L){
+    L=(LinkList)malloc(sizeof(LNode));
+    LinkList s;ElemType x;
+    L->next=NULL;
+    scanf("%d",&x);
+    while(x!=9999){
+        s=(LinkList)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=L->next;
+        L->next=s;
+        scanf("%d",&x);
+    }
+    return true;
+}
+void print_list(LinkList L)
+{
+
+    L=L->next;
+
+    while(L!=NULL)
+
+    {
+
+        printf("%d",L->data);//打印当前结点数据
+
+        L=L->next;//指向下一个结点
+
+        if(L!=NULL)
+
+        {
+
+            printf(" ");
+
+        }
+
+    }
+
+    printf("\n");
+
+}
+void list_tail_insert(LinkList &L)
+{
+    ElemType x;
+    L=(LinkList)malloc(sizeof(LNode));
+    L->data=NULL;
+    LinkList s,r=L;
+    scanf("%d",&x);
+    while (x!=9999)
+    {
+        s=(LinkList)malloc(sizeof(LNode));
+        s->data=x;
+        s->next=r->next;
+        r->next=s;
+        r=s;
+        scanf("%d",&x);
+    }
+
+}
+LinkList GetElem(LinkList L,ElemType e)
+{
+    ElemType i=1;
+    LinkList p=L->next;
+    if (e==0)
+    {
+        return L;
+    }
+    if (e<1)
+    {
+        return NULL;
+    }
+    while (p&&i<e)
+    {
+        p=p->next;
+        i++;
+    }
+    return p;
+}
+LinkList LocateElem(LinkList L,ElemType e)
+{
+    LinkList p=L->next;
+    while (p!=NULL&&p->data!=e){
+        p=p->next;}
+    return p;
+}
+bool ListFrontInsert(LinkList &L,int i,ElemType e)
+{
+    LinkList p=GetElem(L,i-1);
+    if(p==NULL)
+    {
+        return false;
+    }
+    LinkList s=(LinkList)malloc(sizeof(LNode));
+    s->data=e;
+    s->next=p->next;
+    p->next=s;
+    return true;
+}
+bool ListDelete(LinkList &L,ElemType i)
+{
+    LinkList p=GetElem(L,i-1);
+    if(p==NULL)
+    {
+        return false;
+    }
+    LinkList q=p->next;
+    if (q==NULL)
+    {
+        return false;
+    }
+    p->next=q->next;
+    free(q);
+    return true;
+}
+
+int main(){
+    LinkList L,S;
+    list_head_insert(S);
+    print_list(S);
+    list_tail_insert(L);
+
+    print_list(L);
+
+    return 0;
+}
